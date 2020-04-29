997f747646d7d265209ffb504bcab776cf5f59c4
Elodie <elodie.etienne96@gmail.com>
2020-04-29 09:00:57 +0200

    Preprocc + bleu

--- preprocessing.py
+++ preprocessing.py
@@ -1,68 +1,25 @@
-# Some preprocesssing that will be common to all the text classification methods you will see. 
-
+###################
+#Import libraries
 import pandas as pd
 import re
 import string
-import nltk
-#nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
+#import nltk
+#from nltk.tokenize import word_tokenize
+import pickle
 
-
+#Import dataset 
 dataset = pd.read_excel(r'C:\Users\elodi\dox\Documents\Doctorat\Formation doctorale\Cours suivis\Deep learning\Projet Deep Learning\dnd-backgrounds-generator\Data\dd_bios.xls',encoding='latin1')
-print(dataset)
+#print(dataset)
 dataset.columns = ['Time', 'Name', 'Race', 'Class', 'Backstory']
 
 #Delete rows with missing elements
 dataset=dataset.dropna()
 dataset=dataset.reset_index(drop=True)
-print(dataset)
-
-#def remove_numbers(str):
-#    string_no_numbers = re.sub("\d+", "_Number_", str)
-#    return string_no_numbers
-##
-#def remove_line(str):
-#    string_= re.sub("\n", " ", str)
-#    string_= re.sub("\r", " ", string_)
-#    return string_
-#
-#
-#def strip_html_tags(text):
-#    """remove html tags from text"""
-#    soup = BeautifulSoup(text, "html.parser")
-#    stripped_text = soup.get_text(separator=" ")
-#    return stripped_text
-#
-#def remove_website(text):
-#    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-#    return text
-#
-#def remove_whitespace(text):
-#    """remove extra whitespaces from text"""
-#    text = text.strip()
-#    return " ".join(text.split())
-#
-#
-#def text_preprocessing(text):
-#    """preprocess text with default option set to true for all steps"""
-#    text= remove_website(text)
-#    text = strip_html_tags(text)
-#    text = text.lower()
-#    text=remove_punctuations(text)
-#    text=remove_numbers(text)
-#    text=remove_line(text)
-#    text = remove_whitespace(text)
-#    return text
-#
-#def unique(list1): 
-#    x = np.array(list1) 
-#    print(np.unique(x)) 
-#
-#
+#print(dataset)
 
 
-
-###########
+#Remove punctuation 
+#This function removes punctuation and English quotation marks from a string called text. However it keeps the hyphen in the text. 
 def remove_punct(text):
     text = text.replace("“", "")
     text = text.replace("”", "")
@@ -74,22 +31,33 @@ def remove_punct(text):
     return stripped
 
 
-
+#Function to apply before the tokenization 
+#This function 
+    #-replaces interrogation and exclamation points by points in the text
+    #-removes quotation marks in the text
+    #-replaces every number by the sring "_Number_" in the text
+    #-replaces  the string name in the text by _Name_
+    #-adds "_end_" at the end of the text
+#The inputs are two strings text and name. 
 def Before_Token(text, name):
     text = text.replace("?", ".")
     text = text.replace("!", ".")
     text = text.replace(".", " .")
     text = text.replace("“", "")
     text = text.replace("”", "")
-    text = re.sub("\d+", "_Number_", text)
-    if name in text: 
-        text = re.sub(name, "_Name_", text)
-    else: 
+    text = re.sub("\d+", "_number_", text)
+    if name in text: #if the string name is in the text
+        text = re.sub(name, "_name_", text) # replaces name by _Name_
+    else: #if not, look if one part of the string is in the text and if yes, replace it by _Name_
         name=remove_punct(name)
         for i in range(0, len(name)):
-            text = re.sub(name[i], "_Name_", text)
+            text = re.sub(name[i], "_name_", text)  
+    text = text + " _end_"
     return text
     
+#Tokenization, words by word, of the string text by keeping the point. 
+#First this function applies the function Before_Token. 
+#The inputs are two strings text and name.
 def Tokenization(text, name):
    text= Before_Token(text, name)
    words = text.split()
@@ -97,11 +65,12 @@ def Tokenization(text, name):
 
 print("#######################################")
 
-
-
-
+#Tokenization of each backstory. 
 output = [None]*len(dataset)
+
+
 #print(output)
+#for i in range(2430,len(dataset)):
 for i in range(0,len(dataset)):
     print("############ Index :", i)
     print(dataset.Backstory[i])
@@ -109,3 +78,10 @@ for i in range(0,len(dataset)):
     output[i]=Tokenization(dataset.Backstory[i], dataset.Name[i]) 
     print(output[i])
  
+# end description 
+
+#Save the output it in a file named 'Preprocessing_file'
+outfile = open('Preprocessing_file','wb')
+pickle.dump(output,outfile)
+outfile.close()
+

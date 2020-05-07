93d20df4afd834826b0293a0d4d23df940e96b39
Florian Peters <fl0wryan@hotmail.com>
2020-05-07 17:07:20 +0200

    Fix unit test

--- unit-tests.py
+++ unit-tests.py
@@ -4,8 +4,7 @@ from dndGAN import LSTMGenerator, GANTrainer
 
 
 if __name__ == '__main__':
-    data = [torch.tensor([0, 1, 2, 3, 4, 5, 6, 7]),
-            torch.tensor([7, 6, 5, 4, 3, 2, 1, 0])]
+    data = [torch.randint(0, 7, size=(10, 8))]
 
     dis = CNNDiscriminator(batch_size=1, max_seq=8, voc_size=8)
     gen = LSTMGenerator(voc_dim=8, lstm_dim=4, embedding_dim=4, max_len=8)
